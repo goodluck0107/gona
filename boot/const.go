834f7ca4ea34c816a1976b687ff95e69f4d8776a
xiongtao <251454910@qq.com>
2025-02-21 16:50:13 +0800

    refact

--- boot/const.go
+++ boot/const.go
@@ -1,16 +1,17 @@
 package boot
 
 const (
-	KeyChannelReadLimit string = "KeyChannelReadLimit" // 连接读取消息长度限制
-	KeyPacketBytesCount string = "KeyPacketBytesCount" // 消息长度占用字节数
-	KeyReadTimeOut      string = "KeyReadTimeOut"      // 连接读取消息超时时间
-	KeyWriteTimeOut     string = "KeyWriteTimeOut"     // 连接写入消息超时时间
+	KeyReadTimeOut  string = "KeyReadTimeOut"  // 连接读取消息超时时间
+	KeyWriteTimeOut string = "KeyWriteTimeOut" // 连接写入消息超时时间
 
-	KeyConnType          string = "KeyConnType"          // 连接类型
-	KeyIP                string = "KeyIP"                // 连接IP
-	KeyPort              string = "KeyPort"              // 连接端口
-	KeyURLPath           string = "URLPath"              // 请求路径
+	KeyChannelReadLimit  string = "KeyChannelReadLimit"  // 连接读取消息长度限制
+	KeyPacketBytesCount  string = "KeyPacketBytesCount"  // 消息长度占用字节数
 	KeyIsLD              string = "KeyIsLD"              // 是否小端
 	KeyLengthInclude     string = "KeyLengthInclude"     // 包长度是否包含自己的长度
 	SkipPacketBytesCount string = "SkipPacketBytesCount" // 是否跳过包长度
+
+	KeyConnType string = "KeyConnType" // 连接类型
+	KeyIP       string = "KeyIP"       // 连接IP
+	KeyPort     string = "KeyPort"     // 连接端口
+	KeyURLPath  string = "URLPath"     // 请求路径
 )
